dee7ce1628df96f090b948b2293bf8ec16698a79
zaminku <zaminkugshia@gmail.com>
2021-04-02 14:00:04 -0500

    Updated initialize and parent=

--- w3d5/PolyTreeNode/lib/00_tree_node.rb
+++ w3d5/PolyTreeNode/lib/00_tree_node.rb
@@ -1,3 +1,25 @@
 class PolyTreeNode
 
+    attr_reader :value, :parent
+    attr_accessor :children
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        @parent = node
+        if !node.nil? && !node.children.include?(self)
+            node.children << self
+        end
+    end
+
+    def add_child
+
+    end
+
+    def remove_child
+
+    end
 end
