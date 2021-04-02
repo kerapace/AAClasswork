6d3e7e49666e6bd68e02d2c290ef3be4000a2919
kerapace <jhill01@manhattan.edu>
2021-04-02 17:38:55 -0400

    got valid_moves and new_move_positions working

--- w3d5/KnightPathFinder/KnightPathFinder.rb
+++ w3d5/KnightPathFinder/KnightPathFinder.rb
@@ -3,12 +3,17 @@ require_relative "../PolyTreeNode/lib/00_tree_node.rb"
 class KnightPathFinder
     def initialize(pos)
         @root_node = PolyTreeNode.new(pos)
+        @considered_positions = [pos]
     end
 
-    def self.new_move_positions(pos)
+    def new_move_positions(pos)
+        new_positions = KnightPathFinder.valid_moves(pos).reject {|p| @considered_positions.include?(p)}
+        @considered_positions += new_positions
+        new_positions
+    end
+
+    def self.valid_moves(pos)
         knight_off_sets = [[1, 2], [2, 1], [-1, 2], [-2, 1], [1, -2], [2, -1], [-1, -2], [-2, -1]]
-        new_positions = knight_off_sets.map {|a| a.zip(pos).inject(:+)}
-
+        knight_off_sets.map {|a| a.zip(pos).map(&:sum)}
     end
-
 end
