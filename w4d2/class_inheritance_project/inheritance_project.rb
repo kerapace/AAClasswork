7495cd7dc063c2c59669265f32c880432f201db3
kerapace <jhill01@manhattan.edu>
2021-04-06 16:56:42 -0400

    finished first draft of class inheritance project

--- w4d2/class_inheritance_project/inheritance_project.rb
+++ w4d2/class_inheritance_project/inheritance_project.rb
@@ -1,7 +1,38 @@
 class Employee
+    attr_reader :name, :title, :salary, :boss,
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = boss
+        @boss = boss
+    end
 
+    def bonus(multiplier)
+        @bonus = self.salary * multiplier
+    end
 end
 
 class Manager < Employee
+    attr_accessor :employees
+    def initialize(name, title, salary, boss)
+        @employees = []
+        super(name, title, salary, boss)
+    end
 
+    def bonus(multiplier)
+        @bonus = 0
+        @employees.each do |emp|
+            if emp.is_a?(Manager)
+                @bonus += emp.bonus(multiplier)
+            else
+                @bonus += emp.salary * multiplier
+            end
+        end
+        @bonus
+    end
 end
+
+ned = Manager.new("Ned","Founder",1000000,nil)
+darren = Manager.new("Darren","TA Manager",78000,"Ned")
+shawna = Employee.new("Shawna","TA",12000,"Darren")
+david = Employee.new("David","TA",10000,"Darren")
