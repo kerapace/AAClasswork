8a229659bc4f79c0f9c258b7a8e5692756d24aed
ziosanfer <ziosanfer@gmail.com>
2021-04-06 14:50:28 -0700

    started writing piece class

--- w4d2/chess_project/piece.rb
+++ w4d2/chess_project/piece.rb
@@ -1,3 +1,26 @@
 class Piece
+    attr_accessor :position
+    attr_reader :side
+
+    def initialize(side, board, position)
+        @side = side
+        @board = board
+        @position = position
+    end
+
+    def empty?
+        return false
+    end
+
+    def valid_moves
+    end
+
+    def moves
+        []
+    end
+
+end
+
+module Stepable
     
 end
