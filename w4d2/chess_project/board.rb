af9b9a11a497752bdee527e139bbe9f97a6163cf
kerapace <jhill01@manhattan.edu>
2021-04-06 18:06:26 -0400

    updated stepable module

--- w4d2/chess_project/board.rb
+++ w4d2/chess_project/board.rb
@@ -18,10 +18,15 @@ class Board
     def move_piece(start_pos, end_pos)
         if self[start_pos] == :empty
             raise Exception.new("There's no piece to move!")
-        # elsif self[start_pos].can_move?(start_pos,end_pos)
+        # elsif self[start_pos].valid_move?(start_pos,end_pos)
         #     raise Exception.new("This piece can't move there!")
         end
         self[start_pos], self[end_pos] = :empty, self[start_pos]
+    end
+
+    def self.valid_move?(pos)
+        row, col = pos
+        row >= 0 && row < 8 && col >= 0 && col < 8
     end
     private
     def set_up_board
--- w4d2/chess_project/piece.rb
+++ w4d2/chess_project/piece.rb
@@ -1,3 +1,5 @@
+require 'board'
+
 class Piece
     attr_accessor :position
     attr_reader :side
@@ -15,12 +17,23 @@ class Piece
     def valid_moves
     end
 
-    def moves
-        []
-    end
-
 end
 
 module Stepable
+    def moves
+        poss_moves = []
+        curr_row, curr_col = self.position
+        move_diffs.each do |row,col|
+            poss_pos = [curr_row + row, curr_col + col]
+            poss_moves << poss_pos if Board.valid_move?(poss_pos)
+        end
+        poss_moves
+    end
     
+    def move_diffs
+        []
+    end
+end
+
+module Slideable
 end
