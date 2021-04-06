58f722180f84c110b8d12dc3729d4e242940ad77
kerapace <jhill01@manhattan.edu>
2021-04-06 17:31:24 -0400

    did first pass of Board class

---
+++ w4d2/chess_project/board.rb
@@ -0,0 +1,31 @@
+require_relative 'piece'
+
+class Board
+    def initialize
+        @grid = Array.new(8) {Array.new(8,:empty)}
+    end
+
+    def [](pos)
+        row, col = pos
+        @grid[row][col]
+    end
+
+    def []=(pos, val)
+        row, col = pos
+        @grid[row][col] = val
+    end
+
+    def move_piece(start_pos, end_pos)
+        if self[start_pos] == :empty
+            raise Exception.new("There's no piece to move!")
+        # elsif self[start_pos].can_move?(start_pos,end_pos)
+        #     raise Exception.new("This piece can't move there!")
+        end
+        self[start_pos], self[end_pos] = :empty, self[start_pos]
+    end
+    private
+    def set_up_board
+
+    end
+end
+
