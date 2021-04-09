e3bd91cf6912fdbb88a3701f5045d47e0f84f0d0
kerapace <jhill01@manhattan.edu>
2021-04-08 23:14:12 -0400

    fixed some glitches, need to try making this run somewhat faster

---
+++ w4d2/chess_project/player.rb
@@ -0,0 +1,25 @@
+class Player
+    attr_reader :side, :cursor
+    def initialize(p_side,display)
+        @side = p_side
+        @display = display
+        @cursor = display.cursor
+    end
+
+    def get_input
+        init_loc, final_loc = nil, nil
+        system("clear")
+        @display.render
+        while init_loc.nil? || final_loc.nil?
+            val = cursor.get_input
+            if init_loc.nil?
+                init_loc = val
+            elsif final_loc.nil?
+                final_loc = val
+            end
+            system("clear")
+            @display.render
+        end
+        [init_loc, final_loc]
+    end
+end
