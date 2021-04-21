2262a36c506a07be3c45d12a5e95280869d39768
kerapace <jhill01@manhattan.edu>
2021-04-21 17:52:16 -0400

    mocked up show, tested taking in form input

--- W6D3/first_controller_project/first_controller/app/controllers/users_controller.rb
+++ W6D3/first_controller_project/first_controller/app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-    
+
     def index
         render plain: "I'm in the index action!"
     end
 
     def create
         render json: params
+    end
+
+    def show
+        render body: raw
     end
 end
