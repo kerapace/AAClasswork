f008e87f7204eab9d56c323609e2de26f5dcdef0
Kien Nguyen <nguyen_kien@live.com>
2021-04-21 18:26:33 -0500

    finished route project

--- W6D3/first_controller_project/first_controller/app/controllers/users_controller.rb
+++ W6D3/first_controller_project/first_controller/app/controllers/users_controller.rb
@@ -19,9 +19,23 @@ class UsersController < ApplicationController
         render json: user
     end
 
-    def create
-        user = User.new(params.require(:user).permit(:username,:email))
-        user.save!
-        render json: user
-    end
+   def update
+         user =  User.find_by(id: params[:id])
+
+        if user.update(params.require(:user).permit(:username,:email))
+            redirect_to "/users/#{user.id}"
+        else
+            render json: user.errors.full_messages, status: 422
+        end
+   end
+
+   def destroy
+        user =  User.find_by(id: params[:id])
+        if user.destroy
+            redirect_to "/users"
+        else
+            render json: user.errors.full_messages, status: 422
+        end
+   end
+
 end
