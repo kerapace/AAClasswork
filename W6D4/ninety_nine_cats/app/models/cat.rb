1eca3e020f98bbc6517d6591a84152f5015b50d5
Schploink <thekpoc@gmail.com>
2021-04-23 14:00:50 -0700

    begin cats index n stuff

--- W6D4/ninety_nine_cats/app/models/cat.rb
+++ W6D4/ninety_nine_cats/app/models/cat.rb
@@ -2,7 +2,9 @@ require 'action_view'
 
 class Cat < ApplicationRecord
     include ActionView::Helpers::DateHelper
-
+    validates :color, inclusion: {in: %w(brown black orange), message: "invalid color"} 
+    validates :sex, inclusion: {in: %w(M F), message: "must be M or F"}
+    
     def age
         return distance_of_time_in_words(Date.today,self.birth_date)
     end
