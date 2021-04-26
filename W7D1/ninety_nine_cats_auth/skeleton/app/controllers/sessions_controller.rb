d035f173810436ccfca7222dab204f9280e170a2
Michael Lau <lau.michael2@gmail.com>
2021-04-26 13:59:02 -0700

    Add sessions_controller #create

--- W7D1/ninety_nine_cats_auth/skeleton/app/controllers/sessions_controller.rb
+++ W7D1/ninety_nine_cats_auth/skeleton/app/controllers/sessions_controller.rb
@@ -1,13 +1,24 @@
 class SessionsController < ApplicationController
   def new
-
+    render :new
   end
 
   def create
-
+    u = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if u
+      login(u)
+      redirect_to cats_url
+    else
+      render :new
+    end
   end
 
   def destroy
+    
+  end
 
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
