2b6a04a8b74012922377480f5afba9f294a60e96
kerapace <jhill01@manhattan.edu>
2021-04-28 18:18:39 -0400

    mocked up controller and controller specs

--- W7D3/goals_app/spec/requests/users_spec.rb
+++ W7D3/goals_app/spec/requests/users_spec.rb
@@ -11,15 +11,22 @@ RSpec.describe "Users", type: :request do
   describe "GET /new" do 
     it "should render the new user's template" do 
       get :new 
-      expect(response).to render_template(:new)
+      expect(response).to render_template("new")
     end
   end
 
   describe "POST /create" do 
     context "with valid params" do 
-      it "should redirect user to user's index on success" do 
+      it "should redirect user to their userpage on success" do 
         post :create, params: {user: {username: "Test", password: "testing"}}
-        expect(response).to redirect_to(users_url)
+        user = User.find_by(username: "Test")
+        expect(response).to redirect_to(user_url(user))
+      end
+
+      it "should log in the user" do
+        post :create, params: {user: {username: "Test", password: "testing"}}
+        user = user.find_by(username: "Test")
+        expect(session[:session_token]).to eq(user[:session_token])
       end
     end
     context "with invalid params" do 
@@ -36,4 +43,14 @@ RSpec.describe "Users", type: :request do
     end
   end
 
+  before(:all) {
+    user = User.new(username: "alisonwonderland", password: "palindromordnilap")
+    user.save
+  }
+  describe "GET /show" do
+    it "should show a specific user's userpage" do
+      get :show, params: {id: user.id}
+      expect(response).to render_template(:show) 
+    end
+  end
 end
