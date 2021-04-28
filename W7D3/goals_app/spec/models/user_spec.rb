533ba992e3adac20267967bceab634bf4cb62775
StevenTran91 <sdtran91@gmail.com>
2021-04-28 12:02:38 -0700

    started find_by_credentials

--- W7D3/goals_app/spec/models/user_spec.rb
+++ W7D3/goals_app/spec/models/user_spec.rb
@@ -8,9 +8,23 @@ RSpec.describe User, type: :model do
         it {should validate_presence_of(:session_token)}
     end
 
-    subject(:user) {User.new}
-    context "if username is not unique"
-        it "should return error" {}
+    subject(:user) {User.find_by_credentials("Test", "testing")}
+        it {should validate_uniqueness_of(:username)}
     end
+    before(:all) do 
+        User.destroy
+        User.new(username: "Test", password: "testing").save 
+    end
+    describe "::find_by_credentials" do 
+        it "should find extant database entries" do 
+            expect(User.find_by_credentials(user.username, "testing")).to eq(user)
+        end
+        it "it should return nil if there is no such entry in the database" do 
+            expect(User.find_by_credentials("Test", "oranges")).to be(nil)
+            expect(User.find_by_credentials("cat", "testing")).to be(nil)
+        end
+    end
+        
+
 
 end
