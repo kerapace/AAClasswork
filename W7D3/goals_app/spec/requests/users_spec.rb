30837acbebd4aaa5495d6fe5f9587465788d9840
StevenTran91 <sdtran91@gmail.com>
2021-04-28 14:50:30 -0700

    completed index, new, create

--- W7D3/goals_app/spec/requests/users_spec.rb
+++ W7D3/goals_app/spec/requests/users_spec.rb
@@ -4,11 +4,36 @@ RSpec.describe "Users", type: :request do
   describe "GET /index" do
     it "should render the index page" do
         get :index
-        expect(page).to render(:index)
-    end
+        expect(response).to render_template(:index)
+    end 
+  end
 
-    it "should store users in an instance variable" do
-      
+  describe "GET /new" do 
+    it "should render the new user's template" do 
+      get :new 
+      expect(response).to render_template(:new)
     end
   end
+
+  describe "POST /create" do 
+    context "with valid params" do 
+      it "should redirect user to user's index on success" do 
+        post :create, params: {user: {username: "Test", password: "testing"}}
+        expect(response).to redirect_to(users_url)
+      end
+    end
+    context "with invalid params" do 
+      it "validates the presence of username and password" do 
+        post :create, params: {user: {username: "Test", password: nil}}
+        expect(response).to render_template(:new)
+        should set_flash.now[:errors]
+      end
+      it "validates that the password is atleast 6 characters long" do 
+        post :create, params: {user: {username: "Test", password: "cat"}}
+        expect(response).to render_template(:new)
+        should set_flash.now[:errors]
+      end
+    end
+  end
+
 end
