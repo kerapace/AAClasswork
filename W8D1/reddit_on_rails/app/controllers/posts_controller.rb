01cb885c2e6683a618563bdb647741bb51522c75
kerapace <jhill01@manhattan.edu>
2021-05-03 16:30:38 -0400

    finished sub controller, began push controller

--- W8D1/reddit_on_rails/app/controllers/posts_controller.rb
+++ W8D1/reddit_on_rails/app/controllers/posts_controller.rb
@@ -1,2 +1,27 @@
 class PostsController < ApplicationController
+
+    def new
+        @post = Post.new
+        render :new
+    end
+
+    def create
+        @post = Post.new(post_params)
+        @post.author_id = current_user.id
+        @post.sub_id = params[:sub_id]
+        if @post.save
+            redirect_to sub_post_url(@post.sub_id,@post.id)
+        else
+            flash[:errors]
+            redirect_to sub_url(@post.sub_id)
+        end
+    end
+
+    def destroy
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title,:content)
+    end
 end
--- W8D1/reddit_on_rails/app/controllers/subs_controller.rb
+++ W8D1/reddit_on_rails/app/controllers/subs_controller.rb
@@ -18,10 +18,44 @@ class SubsController < ApplicationController
         end
     end
 
-    def 
+    def destroy
+        @sub = Sub.find_by(id: params[:id])
+        if @sub && current_user.id == @sub.moderator_id
+            @sub.destroy
+            redirect_to "/"
+        else
+            flash[:errors] = ["You do not have permission to perform this action."]
+            redirect_to sub_url(@sub)
+        end
+    end
+
+    def edit
+        @sub = Sub.find_by(id: params[:id])
+        render :edit
+    end
+
+    def update
+        @sub = Sub.find_by(id: params[:id])
+        if @sub && @sub.update(sub_params)
+            redirect_to sub_url(@sub)
+        else
+            flash[:errors] = @sub.errors.full_messages
+            render :edit
+        end
+    end
+
+    def show
+        @sub = Sub.find_by(id: params[:id]).includes(:posts)
+        render :show
+    end
+
+    def index
+        @subs = Sub.all
+        render :index
+    end
 
     private
-    
+
     def sub_params
         params.require(:sub).permit(:title, :description)
     end
