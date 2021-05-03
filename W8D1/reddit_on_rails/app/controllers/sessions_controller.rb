c43263c864d8ec59ad6e5bfc02af4461d9c74ab0
Kaz DeBear <kazdb@me.com>
2021-05-03 10:41:24 -0700

    Complete UsersController & SessionsController

--- W8D1/reddit_on_rails/app/controllers/sessions_controller.rb
+++ W8D1/reddit_on_rails/app/controllers/sessions_controller.rb
@@ -1,2 +1,26 @@
 class SessionsController < ApplicationController
+
+    before_action :require_login, only: :destroy
+    before_action :require_logout, only: :create
+
+    def new
+        render :new
+    end
+
+    def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if @user
+            login!(@user)
+            redirect_to users_url
+        else
+            flash[:errors] =  ['Invalid Username or Password']
+            render :new
+        end
+    end
+
+    def destroy
+        logout! if logged_in?
+        redirect_to new_session_url
+    end
+
 end
--- W8D1/reddit_on_rails/app/controllers/users_controller.rb
+++ W8D1/reddit_on_rails/app/controllers/users_controller.rb
@@ -1,2 +1,45 @@
 class UsersController < ApplicationController
+
+    def new
+        render :new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            redirect_to users_url
+        else
+            flash[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render :show
+    end
+
+    def index
+        @users = User.all
+        render :index
+    end
+
+    def destroy
+        @user = User.find_by(id: params[:id])
+        if @user == current_user
+            logout!
+            @user.destroy
+        else
+            
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
+
 end
