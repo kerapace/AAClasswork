2709c74abf0e1990b24e1e744f132b18add0851f
Jennifer Tran <cococlone@gmail.com>
2021-05-10 16:41:46 -0700

    Create flappy_bird

---
+++ W9D1/flappy_bird/src/game.js
@@ -0,0 +1,19 @@
+const Level = require('./level');
+
+export default class FlappyBird {
+  constructor(canvas){
+    this.ctx = canvas.getContext("2d");
+    this.dimensions = { width: canvas.width, height: canvas.height };
+    this.level = new Level(this.dimensions);
+  }
+
+  animate() {
+    this.level.animate();
+  }
+
+  restart() {
+    newLevel = new Level(this.dimensions);
+    this.level = newLevel;
+    this.animate();
+  }
+}
