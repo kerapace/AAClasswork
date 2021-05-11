bb54c2b26e47acfa0d9d0808e8e410d8ac0be9a3
Michael Lau <lau.michael2@gmail.com>
2021-05-11 10:40:20 -0700

    Add draw to MovingObject

--- W9D2/src/index.js
+++ W9D2/src/index.js
@@ -1,1 +1,15 @@
+const MovingObject = require("./moving_object.js");
+
+document.addEventListener("DOMContentLoaded", function() {
+  let canvas = document.getElementById("asteroids-window");
+  const ctx = canvas.getContext("2d");
+  let mo = new MovingObject({
+    pos: [30, 30],
+    vel: [10, 10],
+    radius: 20,
+    color: "#00FF00"
+  });
+  mo.draw(ctx);
+});
+
 console.log("Webpack is working!");
