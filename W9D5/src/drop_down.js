84c55c27d405f6dcff42ee097db5e6d9d722d4f5
msaia97 <masonsaia@gmail.com>
2021-05-14 15:04:32 -0700

    Tried listing dogs

--- W9D5/src/drop_down.js
+++ W9D5/src/drop_down.js
@@ -9,3 +9,25 @@ const dogs = {
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
 
+ function dogLinkCreator() {
+  let dogLinks = [];
+  for(const [name, url] of Object.entries(dogs)){
+    let anchor = document.createElement('a');
+    anchor.innerHTML = name;
+    anchor.href = url;
+    let newLi = document.createElement('li');
+    newLi.appendChild(anchor);
+    dogLinks.push(newLi);
+  }
+  return dogLinks;
+}
+
+function attachDogLinks() {
+  let dogLinks = dogLinkCreator();
+  let dogContainer = document.querySelector('ul.drop-down-dog-list')
+  dogLinks.forEach(el => {
+    dogContainer.appendChild(el);
+  })
+}
+
+attachDogLinks();
--- W9D5/src/index.js
+++ W9D5/src/index.js
@@ -1,2 +1,3 @@
 import warmUp from "./warmup";
 import clock from "./clock.js";
+import attachDogLinks from "./drop_down.js";
