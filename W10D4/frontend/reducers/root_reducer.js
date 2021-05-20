77ae357f751c5b377c99621628879554296ac481
kerapace <jhill01@manhattan.edu>
2021-05-20 18:16:54 -0400

    finished W10D4

--- W10D4/frontend/reducers/root_reducer.js
+++ W10D4/frontend/reducers/root_reducer.js
@@ -1,6 +1,7 @@
-import TodosReducer from "./todos_reducer.js";
+import todosReducer from "./todos_reducer.js";
+import stepsReducer from "./steps_reducer.js";
 import {combineReducers} from "redux";
 
-const rootReducer = combineReducers({todos: TodosReducer});
+const rootReducer = combineReducers({todos: todosReducer, steps: stepsReducer});
 
 export default rootReducer;
--- W10D4/frontend/reducers/todos_reducer.js
+++ W10D4/frontend/reducers/todos_reducer.js
@@ -1,4 +1,4 @@
-import * as Todo from "../actions/todo_actions";
+import {RECEIVE_TODO, RECEIVE_TODOS, REMOVE_TODO} from "../actions/todo_actions";
 
 const initialState = {
   1: {
@@ -19,12 +19,12 @@ const todosReducer = (state = initialState, action) => {
   Object.freeze(state);
   let newState = {};
   switch (action.type) {
-    case Todo.RECEIVE_TODOS:
+    case RECEIVE_TODOS:
       action.todos.forEach(todo => {
         newState[todo.id] = todo;
       });
       return newState;
-    case Todo.RECEIVE_TODO:
+    case RECEIVE_TODO:
       Object.assign(newState, state);
       if (action.todo.id === undefined) {
         let newId = Math.max(0, Object.keys(state)) + 1;
@@ -32,6 +32,10 @@ const todosReducer = (state = initialState, action) => {
       } else {
         newState[action.todo.id] = action.todo;
       }
+      return newState;
+    case REMOVE_TODO:
+      Object.assign(newState, state);
+      delete newState[action.todo.id];
       return newState;
     default:
       return state;
