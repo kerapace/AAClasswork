5e130d7339f28c8568f9dec6c0994de3900c8fc6
kerapace <jhill01@manhattan.edu>
2021-05-20 20:34:18 -0400

    finished for the day

--- W10D4/frontend/components/app.jsx
+++ W10D4/frontend/components/app.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 // import configureStore from "./store/store.js";
 
 const App = (props) => (
-  <h1>Leet app!!!1</h1>
+  <div>
+    <ToDoList />
+  </div>
 );
 
 export default App;
---
+++ W10D4/frontend/components/todos/todo_list.jsx
@@ -0,0 +1,5 @@
+const ToDoList = (props) => (
+  <h3>Todo List goes here!</h3>
+);
+
+export default ToDoList;
