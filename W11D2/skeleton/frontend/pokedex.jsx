b033f352de9055df9b955f652253aa940f5c6505
kerapace <jhill01@manhattan.edu>
2021-05-26 11:57:59 -0400

    finalizing

--- W11D2/skeleton/frontend/pokedex.jsx
+++ W11D2/skeleton/frontend/pokedex.jsx
@@ -4,11 +4,18 @@ import configureStore from "./store/store.js";
 import {fetchAllPokemon} from "./util/api_util.js";
 import {selectAllPokemon} from './reducers/selectors';
 
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Root from "./components/root.jsx";
+
 document.addEventListener("DOMContentLoaded", () => {
   const store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
-  window.receiveAllPokemon = receiveAllPokemon;
-  window.requestAllPokemon = requestAllPokemon;
-  window.selectAllPokemon = selectAllPokemon;
+  const rootElement = document.getElementById("root");
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+  // window.receiveAllPokemon = receiveAllPokemon;
+  // window.requestAllPokemon = requestAllPokemon;
+  // window.selectAllPokemon = selectAllPokemon;
+  ReactDOM.render(<Root store={store}/>,rootElement);
 });
